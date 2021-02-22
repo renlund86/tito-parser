71f2a5102045995a09f72b1aac37190fa947cb56
Mathias Renlund <renlund86@gmail.com>
2021-02-22 16:05:49 +0200

    Added dotenv library for more dynamic setups (CORS setting at this point)

--- public/index.php
+++ public/index.php
@@ -3,11 +3,27 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Exception\HttpNotFoundException;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../classes/TITO.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+$dotenv->load();
+
 $app = AppFactory::create();
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', $_SERVER['CORS_ALLOWED_DOMAIN'])
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 $app->post('/parse', function (Request $request, Response $response, $args) {
     
@@ -29,5 +45,4 @@ $app->post('/parse', function (Request $request, Response $response, $args) {
 });
 
 $errorMiddleware = $app->addErrorMiddleware(false, false, false);
-
 $app->run();
